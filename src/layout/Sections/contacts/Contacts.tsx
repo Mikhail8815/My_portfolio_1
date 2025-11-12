f18c58d740e9967c65d960e48036092806c2b1c5
Mikhail8815 <mishutka150588@mail.ru>
2025-11-13 00:05:46 +0300

    refactor: package.json

--- src/layout/Sections/contacts/Contacts.tsx
+++ src/layout/Sections/contacts/Contacts.tsx
@@ -1,4 +1,4 @@
-import React, { ElementRef, useRef } from "react";
+import React, { useRef } from "react";
 import { SectionTitle } from "../../../components/SectionTitle";
 import { Button } from "../../../components/Button";
 import { Container } from "../../../components/Container";
